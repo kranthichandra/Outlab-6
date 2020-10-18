278e89ec9f8b567edea8d94b0d884f7de6dabf1d
qwerty140 <70227092+qwerty140@users.noreply.github.com>
2020-10-18 23:45:09 +0530

    Add files via upload

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,10 +1,7 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
 import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
-import { getLocaleDateFormat } from '@angular/common';
 
 @Injectable({ providedIn: 'root' })
 export class HeroService {
@@ -18,35 +15,14 @@ headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 
 getData() : Observable<any>{
   return this.http.get<any>(this.heroesUrl)
-  .pipe(
-    tap(_ => this.log('fetched data')),
-        catchError(this.handleError<any>('getData'))
-  );
+
   }
 
 postData(form) : Observable<any>{
 
   return this.http.post<any>(this.heroUrl,form, this.httpOptions)
-  .pipe(
-    tap(_ => this.log('posting data')),
-        catchError(this.handleError<any>('postData'))
-  );
-}
-private handleError<T>(operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
-
-    // TODO: send the error to remote logging infrastructure
-    console.error(error); // log to console instead
-
-    // TODO: better job of transforming error for user consumption
-    this.log(`${operation} failed: ${error.message}`);
-
-    // Let the app keep running by returning an empty result.
-    return of(result as T);
-  };
 
 }
-private log(message: string) {
 
-}
+
 }
